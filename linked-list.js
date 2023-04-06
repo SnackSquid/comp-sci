fb0252e98248f7dfc876102d0fa201f9e1a9295e
snacksquid <spilliams@gmail.com>
2023-04-05 22:11:23 -0500

    add factory functions for linked list and node classes

---
+++ linked-list.js
@@ -0,0 +1,21 @@
+// import section
+
+// factory function to create node class
+const Node = () => {
+  // default null values
+  const nodeValue = null;
+  const nextNodeValue = null;
+  // ability to return the info
+  const value = () => nodeValue;
+  const nextNode = () => nextNodeValue
+}
+
+// factory function to create linked lists
+const LinkedList = () => {
+  
+  const append = (list, value) => {
+    
+  }
+  
+}
+
